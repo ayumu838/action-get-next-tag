4ccc3c5e1b8f86db0f10c346a4a33031b1581845
ayumu.mochizuki <ayumu839.a@gmail.com>
2020-05-08 23:43:36 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ latest_tag=$(git tag | sort -r | head -n 1)
 today=$(date "+%Y%m%d")
 tag_date=${latest_tag:0:8}
 
-if [ $tag_date = $today ]; then
+if [ "$tag_date"= "$today" ]; then
     tag_number=${latest_tag:9:10}
     next_number=`printf '%02d' $((tag_number + 1))`
 
